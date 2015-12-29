b8180f6aec5fd9fb517dd761ef3282bf79b0d0cd
Alexander Ryasnyansky <aryasnyansky@smartlabs.tv>
2015-12-29 16:37:01 +0300

    improve for IDEA + add taskbar applets for network and volti

--- .i3/autostart.sh
+++ .i3/autostart.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 i3-msg --no-startup-id exec ~/.xsessionrc
 i3-msg --no-startup-id exec unity-settings-daemon # set unity theme
+i3-msg exec --no-startup-id nm-applet
+i3-msg-exec --no-startup-id volti
 i3-msg exec --no-startup-id ~/.i3/workspace.sh
 i3-msg exec ~/soft/idea/idea15.sh
 i3-msg exec subl
