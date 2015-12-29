4cfefeccf78b984cfcab5c80db2adf2e3105dd98
Alexander Ryasnyansky <aryasnyansky@smartlabs.tv>
2015-12-29 18:50:44 +0300

    in RU mode keybindings not work

--- .i3/autostart.sh
+++ .i3/autostart.sh
@@ -2,7 +2,7 @@
 i3-msg --no-startup-id exec ~/.xsessionrc
 i3-msg --no-startup-id exec unity-settings-daemon # set unity theme
 i3-msg exec --no-startup-id nm-applet
-i3-msg-exec --no-startup-id volti
+i3-msg exec --no-startup-id volti
 i3-msg exec subl
 i3-msg exec firefox
 i3-msg exec chromium-browser
