a6dfa41845b13b225b8ea782834be3a2a96c8ebc
Alexander Ryasnyansky <aryasnyansky@smartlabs.tv>
2015-12-30 14:40:56 +0300

    add xxkb + numLock autostart

--- .i3/autostart.sh
+++ .i3/autostart.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 i3-msg --no-startup-id exec ~/.xsessionrc
 i3-msg --no-startup-id exec unity-settings-daemon # set unity theme
-i3-msg exec --no-startup-id nm-applet
-i3-msg exec --no-startup-id volti
+i3-msg exec --no-startup-id ~/.i3/workspace.sh
 i3-msg exec subl
 i3-msg exec firefox
 i3-msg exec chromium-browser
 i3-msg exec skype
 i3-msg exec pidgin
-i3-msg exec --no-startup-id ~/.i3/workspace.sh
+i3-msg exec --no-startup-id nm-applet
+i3-msg exec --no-startup-id volti
+i3-msg exec --no-startup-id xxkb
